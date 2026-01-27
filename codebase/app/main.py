f2a25b4edcb1b4a8f7583742f99572f2cca0ac64
Favour Tabi <favoureyong6@gmail.com>
2026-01-27 06:54:05 +0100

    Validator Regex

--- codebase/app/main.py
+++ codebase/app/main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends, HTTPException, Response
 from sqlalchemy.orm import Session
 import models
 import schemas
 from database import get_db
+import re
 
 app = FastAPI(
     title="National ID Validator"
@@ -14,16 +15,21 @@ def get_all(db: Session = Depends(get_db)):
     return db.query(models.Citizen).all()
 
 
-@app.post('/', response_model=schemas.CitizenRead)
+@app.post('/')
 def validate(data: str, db: Session = Depends(get_db)):
     id_number = data
-    db_data = db.query(models.Citizen).filter(models.Citizen.id_number==id_number).first()
 
-    if db_data == None:
-        raise HTTPException(status_code=404, detail="No registered ID found with this ID Number")
+    if re.match(r"^CAM-\d{6}", id_number):
+        db_data = db.query(models.Citizen).filter(models.Citizen.id_number==id_number).first()
+        
+        if db_data == None:
+            raise HTTPException(status_code=404, detail="No ID found with this ID Number")
+        
+        return {"Response": "Valid ID"}
+
+    else: 
+        return {"Response": "Invalid ID"}
     
-    return db_data
-
 
 @app.post("/create", response_model=schemas.CitizenRead)
 def create(data: schemas.Citizen, db: Session = Depends(get_db)):
